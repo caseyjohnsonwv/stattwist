c974ff7b060eaad7d1797b2b677f7a1d79efb47a
Casey Johnson <ctj0001@mix.wvu.edu>
2021-03-27 00:26:45 -0400

    store tweet ids in cookie, load tweet html from api, display in webpage

--- api/tw/dashboard.py
+++ api/tw/dashboard.py
@@ -16,13 +16,24 @@ class TopTweetsResponse(BaseModel):
     retrieved_at: datetime
     most_retweeted_ids: List[str]
     most_liked_ids: List[str]
+
+class TweetIdsRequest(BaseModel):
+    access_token: str
+    access_token_secret: str
+    most_liked_ids: str
+    most_retweeted_ids: str
     class Config:
         orm_mode: True
+
+class TweetCardsResponse(BaseModel):
+    most_liked_cards: List[str]
+    most_retweeted_cards: List[str]
 
 router = APIRouter()
 
 @router.post('/dashboard/load', response_model=TopTweetsResponse)
 async def load_top_tweets(tokens: OAuthRequestTokens):
+    #sample_id = "1359180950672977920"
     api = get_api_instance(tokens.access_token, tokens.access_token_secret)
     date_one_year_ago = datetime.now() - timedelta(days=365)
     top_retweets = [{'id_str':None, 'retweets':-1}]*6
@@ -41,7 +52,21 @@ async def load_top_tweets(tokens: OAuthRequestTokens):
     most_retweeted_ids = [x['id_str'] for x in top_retweets]
     most_liked_ids = [x['id_str'] for x in top_likes]
     return {
-        'most_retweeted_ids':most_retweeted_ids,
-        'most_liked_ids':most_liked_ids,
         'retrieved_at':datetime.now(),
+        'most_retweeted_ids':most_retweeted_ids,
+        'most_liked_ids':most_liked_ids
+    }
+
+@router.post('/dashboard/render', response_model=TweetCardsResponse)
+async def load_tweets_html(req: TweetIdsRequest):
+    api = get_api_instance(req.access_token, req.access_token_secret)
+    most_retweeted_cards = []
+    most_liked_cards = []
+    for id in req.most_retweeted_ids.replace('[^\w,]+','').split(','):
+        most_retweeted_cards.append(api.get_oembed(url="https://twitter.com/{}/status/{}".format(api.me().screen_name, id))['html'])
+    for id in req.most_liked_ids.replace('[^\w,]+','').split(','):
+        most_liked_cards.append(api.get_oembed(url="https://twitter.com/{}/status/{}".format(api.me().screen_name, id))['html'])
+    return {
+        'most_retweeted_cards':most_retweeted_cards,
+        'most_liked_cards':most_liked_cards
     }
--- ui/src/components/Dashboard.vue
+++ ui/src/components/Dashboard.vue
@@ -3,7 +3,10 @@
     <p>Loading...</p>
   </div>
   <div v-else class="container">
-    <p>{{ data }}</p>
+    <h1>Most Retweeted</h1>
+    <p><span v-for="tweet in most_retweeted_tweets" :key="tweet" v-html="tweet"></span></p>
+    <h1>Most Liked</h1>
+    <p><span v-for="tweet in most_liked_tweets" :key="tweet" v-html="tweet"></span></p>
   </div>
 </template>
 
@@ -15,23 +18,42 @@ export default {
   data() {
     return {
       pending: true,
-      data: null,
+      most_retweeted_tweets: [],
+      most_liked_tweets: [],
     };
   },
   methods: {
-    async loadTweets() {
-      const path = `${process.env.VUE_APP_API_BASE_URL}/dashboard/load`;
+    async getTweetIds() {
+      const most_retweeted_ids = this.$cookie.get('most_retweeted_ids');
+      const most_liked_ids = this.$cookie.get('most_liked_ids');
+      if (most_retweeted_ids === null || most_liked_ids === null) {
+        // eslint-disable-next-line
+        console.log("API CALL");
+        const path = `${process.env.VUE_APP_API_BASE_URL}/dashboard/load`;
+        const access_token = this.$cookie.get('access_token');
+        const access_token_secret = this.$cookie.get('access_token_secret');
+        const response = await axios.post(path, { access_token, access_token_secret });
+        this.$cookie.set('most_retweeted_ids', response.data.most_retweeted_ids, { expires: '4h' });
+        this.$cookie.set('most_liked_ids', response.data.most_liked_ids, { expires: '4h' });
+      }
+    },
+    async renderTweets() {
+      const path = `${process.env.VUE_APP_API_BASE_URL}/dashboard/render`;
       const access_token = this.$cookie.get('access_token');
       const access_token_secret = this.$cookie.get('access_token_secret');
-      const response = await axios.post(path, { access_token, access_token_secret });
-      // eslint-disable-next-line
-      console.log(response.data);
+      const most_retweeted_ids = this.$cookie.get('most_retweeted_ids');
+      const most_liked_ids = this.$cookie.get('most_liked_ids');
+      const response = await axios.post(path, {
+        access_token, access_token_secret, most_retweeted_ids, most_liked_ids,
+      });
+      this.most_retweeted_tweets = response.data.most_retweeted_cards;
+      this.most_liked_tweets = response.data.most_liked_cards;
       this.pending = false;
-      this.data = response.data;
     },
   },
   async created() {
-    await this.loadTweets();
+    await this.getTweetIds();
+    await this.renderTweets();
   },
 };
 </script>
